4b5eee0e9c28849065e79216c47ae667c94d1528
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-14 18:33:10 +0000

    Feat: Add Route for homepage
    include Homepage component as a child route under MainLayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
 
-export const routes: Routes = [{ path: '', component: MainLayoutComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{ path: '', component: HomepageComponent }],
+  },
+];
