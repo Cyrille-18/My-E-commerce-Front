ff3420a212204607e5637dad71d438e425e00054
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-17 20:20:49 +0000

    feat: Add CategoryCard component to homepage imports

--- src/app/Pages/homepage/homepage.component.ts
+++ src/app/Pages/homepage/homepage.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { ProductsliderComponent } from '../../components/productslider/productslider.component';
 import { PromotionproductComponent } from '../../components/promotionproduct/promotionproduct.component';
+import { CategorycardComponent } from '../../components/categorycard/categorycard.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [ProductsliderComponent, PromotionproductComponent],
+  imports: [
+    ProductsliderComponent,
+    PromotionproductComponent,
+    CategorycardComponent,
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
