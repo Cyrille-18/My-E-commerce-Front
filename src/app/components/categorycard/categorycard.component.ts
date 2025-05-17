df091d43295a864d14519a1e7d48570b19691098
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-17 20:19:53 +0000

    feat: Add CategoryCard component with HTML structure, CSS styles, and unit tests

---
+++ src/app/components/categorycard/categorycard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorycard',
+  imports: [],
+  templateUrl: './categorycard.component.html',
+  styleUrl: './categorycard.component.css'
+})
+export class CategorycardComponent {
+
+}
